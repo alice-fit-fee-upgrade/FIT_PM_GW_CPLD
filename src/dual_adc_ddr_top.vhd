fe9efbf01b8437490a5bd79d124a5a05ba4e179c
rsmalec <rsmalec@kplabs.pl>
2025-08-25 16:22:54 +0200

    modules for final design

--- src/dual_adc_ddr_top.vhd
+++ src/dual_adc_ddr_top.vhd
@@ -13,20 +13,20 @@ entity dual_adc_ddr_top is
     ADCA_CLK_N : in std_logic;
 
     ADCAA_D0_P : in std_logic; ADCAA_D0_N : in std_logic;
-    ADCAA_D1_P : in std_logic; ADCAA_D1_N : in std_logic;
     ADCAA_D2_P : in std_logic; ADCAA_D2_N : in std_logic;
-    ADCAA_D3_P : in std_logic; ADCAA_D3_N : in std_logic;
     ADCAA_D4_P : in std_logic; ADCAA_D4_N : in std_logic;
-    ADCAA_D5_P : in std_logic; ADCAA_D5_N : in std_logic;
     ADCAA_D6_P : in std_logic; ADCAA_D6_N : in std_logic;
+    ADCAA_D8_P : in std_logic; ADCAA_D8_N : in std_logic;
+    ADCAA_D10_P : in std_logic; ADCAA_D10_N : in std_logic;
+    ADCAA_D12_P : in std_logic; ADCAA_D12_N : in std_logic;
 
     ADCAB_D0_P : in std_logic; ADCAB_D0_N : in std_logic;
-    ADCAB_D1_P : in std_logic; ADCAB_D1_N : in std_logic;
     ADCAB_D2_P : in std_logic; ADCAB_D2_N : in std_logic;
-    ADCAB_D3_P : in std_logic; ADCAB_D3_N : in std_logic;
     ADCAB_D4_P : in std_logic; ADCAB_D4_N : in std_logic;
-    ADCAB_D5_P : in std_logic; ADCAB_D5_N : in std_logic;
     ADCAB_D6_P : in std_logic; ADCAB_D6_N : in std_logic;
+    ADCAB_D8_P : in std_logic; ADCAB_D8_N : in std_logic;
+    ADCAB_D10_P : in std_logic; ADCAB_D10_N : in std_logic;
+    ADCAB_D12_P : in std_logic; ADCAB_D12_N : in std_logic;
 
     ADCA_CLK : out std_logic;
     ADC_AA   : out std_logic_vector(ADC_RESOLUTION_BITS-1 downto 0);
@@ -36,20 +36,20 @@ entity dual_adc_ddr_top is
     ADCB_CLK_N : in std_logic;
 
     ADCBA_D0_P : in std_logic; ADCBA_D0_N : in std_logic;
-    ADCBA_D1_P : in std_logic; ADCBA_D1_N : in std_logic;
     ADCBA_D2_P : in std_logic; ADCBA_D2_N : in std_logic;
-    ADCBA_D3_P : in std_logic; ADCBA_D3_N : in std_logic;
     ADCBA_D4_P : in std_logic; ADCBA_D4_N : in std_logic;
-    ADCBA_D5_P : in std_logic; ADCBA_D5_N : in std_logic;
     ADCBA_D6_P : in std_logic; ADCBA_D6_N : in std_logic;
+    ADCBA_D8_P : in std_logic; ADCBA_D8_N : in std_logic;
+    ADCBA_D10_P : in std_logic; ADCBA_D10_N : in std_logic;
+    ADCBA_D12_P : in std_logic; ADCBA_D12_N : in std_logic;
 
     ADCBB_D0_P : in std_logic; ADCBB_D0_N : in std_logic;
-    ADCBB_D1_P : in std_logic; ADCBB_D1_N : in std_logic;
     ADCBB_D2_P : in std_logic; ADCBB_D2_N : in std_logic;
-    ADCBB_D3_P : in std_logic; ADCBB_D3_N : in std_logic;
     ADCBB_D4_P : in std_logic; ADCBB_D4_N : in std_logic;
-    ADCBB_D5_P : in std_logic; ADCBB_D5_N : in std_logic;
     ADCBB_D6_P : in std_logic; ADCBB_D6_N : in std_logic;
+    ADCBB_D8_P : in std_logic; ADCBB_D8_N : in std_logic;
+    ADCBB_D10_P : in std_logic; ADCBB_D10_N : in std_logic;
+    ADCBB_D12_P : in std_logic; ADCBB_D12_N : in std_logic;
 
     ADCB_CLK : out std_logic;
     ADC_BA   : out std_logic_vector(ADC_RESOLUTION_BITS-1 downto 0);
@@ -68,17 +68,14 @@ architecture RTL of dual_adc_ddr_top is
 
 begin
 
-  adc_aa_p <= (ADCAA_D6_P, ADCAA_D5_P, ADCAA_D4_P, ADCAA_D3_P, ADCAA_D2_P, ADCAA_D1_P, ADCAA_D0_P);
-  adc_aa_n <= (ADCAA_D6_N, ADCAA_D5_N, ADCAA_D4_N, ADCAA_D3_N, ADCAA_D2_N, ADCAA_D1_N, ADCAA_D0_N);
-
-  adc_ab_p <= (ADCAB_D6_P, ADCAB_D5_P, ADCAB_D4_P, ADCAB_D3_P, ADCAB_D2_P, ADCAB_D1_P, ADCAB_D0_P);
-  adc_ab_n <= (ADCAB_D6_N, ADCAB_D5_N, ADCAB_D4_N, ADCAB_D3_N, ADCAB_D2_N, ADCAB_D1_N, ADCAB_D0_N);
-
-  adc_ba_p <= (ADCBA_D6_P, ADCBA_D5_P, ADCBA_D4_P, ADCBA_D3_P, ADCBA_D2_P, ADCBA_D1_P, ADCBA_D0_P);
-  adc_ba_n <= (ADCBA_D6_N, ADCBA_D5_N, ADCBA_D4_N, ADCBA_D3_N, ADCBA_D2_N, ADCBA_D1_N, ADCBA_D0_N);
-
-  adc_bb_p <= (ADCBB_D6_P, ADCBB_D5_P, ADCBB_D4_P, ADCBB_D3_P, ADCBB_D2_P, ADCBB_D1_P, ADCBB_D0_P);
-  adc_bb_n <= (ADCBB_D6_N, ADCBB_D5_N, ADCBB_D4_N, ADCBB_D3_N, ADCBB_D2_N, ADCBB_D1_N, ADCBB_D0_N);
+  adc_aa_p <= (ADCAA_D12_P, ADCAA_D10_P, ADCAA_D8_P, ADCAA_D6_P, ADCAA_D4_P, ADCAA_D2_P, ADCAA_D0_P);
+  adc_aa_n <= (ADCAA_D12_N, ADCAA_D10_N, ADCAA_D8_N, ADCAA_D6_N, ADCAA_D4_N, ADCAA_D2_N, ADCAA_D0_N);
+  adc_ab_p <= (ADCAB_D12_P, ADCAB_D10_P, ADCAB_D8_P, ADCAB_D6_P, ADCAB_D4_P, ADCAB_D2_P, ADCAB_D0_P);
+  adc_ab_n <= (ADCAB_D12_N, ADCAB_D10_N, ADCAB_D8_N, ADCAB_D6_N, ADCAB_D4_N, ADCAB_D2_N, ADCAB_D0_N);
+  adc_ba_p <= (ADCBA_D12_P, ADCBA_D10_P, ADCBA_D8_P, ADCBA_D6_P, ADCBA_D4_P, ADCBA_D2_P, ADCBA_D0_P);
+  adc_ba_n <= (ADCBA_D12_N, ADCBA_D10_N, ADCBA_D8_N, ADCBA_D6_N, ADCBA_D4_N, ADCBA_D2_N, ADCBA_D0_N);
+  adc_bb_p <= (ADCBB_D12_P, ADCBB_D10_P, ADCBB_D8_P, ADCBB_D6_P, ADCBB_D4_P, ADCBB_D2_P, ADCBB_D0_P);
+  adc_bb_n <= (ADCBB_D12_N, ADCBB_D10_N, ADCBB_D8_N, ADCBB_D6_N, ADCBB_D4_N, ADCBB_D2_N, ADCBB_D0_N);
 
 
   adc_a_inst : entity adc_ddr_top
